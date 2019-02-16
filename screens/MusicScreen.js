4c637effd953d65a9ca2c6fc2159a0013bacbe7e
DavidWChen <david.willisis.chen@gmail.com>
2019-02-16 10:58:38 -0800

    request attempt

--- screens/MusicScreen.js
+++ screens/MusicScreen.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { ScrollView, Text, View, StyleSheet, Button, TouchableHighlight, Image} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import { Constants, Video, Audio, Asset } from 'expo';
-
-
+clientId = "PmFCVf89FiVPeprYiXRHNA==";
+clientKey = "fKJaIZYiS4cYYCEg8wVSX4XM0UanJ1WuopDLDWjw-C7uv3h_LJZVlHkuHyuYZrtycu_vf-5z1oAvaaRThXHkxg=="
 class Icon {
   constructor(module, width, height) {
     this.module = module;
@@ -88,6 +88,33 @@ export default class LinksScreen extends React.Component {
       this.playSoundtrack();
   }
 
+  var request = require('request');
+  _houndifyRequest = () => {
+    var houndRequest = {
+        ClientID: clientId,
+        RequestID: '{request-id-that-matches-headers}',
+        DeviceID: '8333687f040f3d88',
+        ClientVersion: '1.0',
+        RequestID: uuid.v1(),
+        SessionID: uuid.v1(),
+        TimeZone: 'America/New_York',
+        TimeStamp: {timestamp-that-matches-headers},
+        Language: 'en_US'
+    };
+
+    request({
+        url: 'https://api.houndify.com/v1/audio',
+        headers: {
+            'Hound-Request-Authentication': {clientId},
+            'Hound-Client-Authentication': {clientKey},
+            'Hound-Request-Info': JSON.stringify(houndRequest).
+        },
+        json: true
+    }, function (err, resp, body) {
+        //body will contain the JSON response
+        console.log(body);
+    });
+  }
   render() {
 
     return (
@@ -117,6 +144,50 @@ export default class LinksScreen extends React.Component {
     );
   }
 }
+
+var uuid = require('node-uuid');
+var crypto = require('crypto');
+
+function generateAuthHeaders (clientId, clientKey, userId, requestId) {
+
+    if (!clientId || !clientKey) {
+        throw new Error('Must provide a Client ID and a Client Key');
+    }
+    
+    // Generate a unique UserId and RequestId.
+    userId = userId || uuid.v1();
+
+    // keep track of this requestId, you will need it for the RequestInfo Object
+    requestId = requestId || uuid.v1();
+
+    var requestData = userId + ';' + requestId;
+
+    // keep track of this timestamp, you will need it for the RequestInfo Object
+    var timestamp = Math.floor(Date.now() / 1000),  
+
+        unescapeBase64Url = function (key) {
+            return key.replace(/-/g, '+').replace(/_/g, '/');
+        },
+
+        escapeBase64Url = function (key) {
+            return key.replace(/\+/g, '-').replace(/\//g, '_');
+        },
+
+        signKey = function (clientKey, message) {
+            var key = new Buffer(unescapeBase64Url(clientKey), 'base64');
+            var hash = crypto.createHmac('sha256', key).update(message).digest('base64');
+            return escapeBase64Url(hash);
+
+        },
+
+        encodedData = signKey(clientKey, requestData + timestamp),
+        headers = {
+            'Hound-Request-Authentication': requestData,
+            'Hound-Client-Authentication': clientId + ';' + timestamp + ';' + encodedData
+        };
+
+    return headers;
+};
 
 const styles = StyleSheet.create({
   container: {
