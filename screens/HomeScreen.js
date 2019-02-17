822ffe3ddf7c5256f892ad758f442332ded2b7eb
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-02-16 18:53:34 -0800

    added home screen design

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {
   Image,
   Platform,
+  Button,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
+  TouchableNativeFeedback,
   View,
 } from 'react-native';
-import { WebBrowser } from 'expo';
+import { WebBrowser, Font } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
@@ -17,49 +19,27 @@ export default class HomeScreen extends React.Component {
     header: null,
   };
 
+
+
   render() {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
+          <View style={styles.sleepIconContainer}>
+            <Image source={require('../assets/images/sleepIcon.png')} style={styles.sleepIcon}>
+
+            </Image>
+            <Text style={styles.BetterSleepTitle}>BetterSleep</Text>
+            <Text style={styles.BetterSleepDescription}>- Some interesting stuff ...</Text>
           </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+          <View style={styles.activateButtonContainer} >
+            <TouchableOpacity style={styles.activateButton}>
+              <Text style={styles.activateButtonText}>Play Music</Text>
             </TouchableOpacity>
           </View>
+
+
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
       </View>
     );
   }
@@ -87,102 +67,61 @@ export default class HomeScreen extends React.Component {
     }
   }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000036',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
+  sleepIconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sleepIcon: {
+    flex: 1,
+    marginTop: 30,
+    width: 220,
+    height: 220,
+    resizeMode: 'contain',
+  },
+  BetterSleepTitle: {
+    marginTop: 25,
+    fontSize: 50,
+    color: '#ffe0da',
+    fontFamily: 'coiny',
+  },
+  BetterSleepDescription: {
+    marginTop: 30,
+    fontSize: 18,
+    fontFamily: 'serif',
+    color: '#fff',
+    marginLeft: '10%',
+    marginRight: '10%',
     textAlign: 'center',
+    lineHeight: 30
   },
   contentContainer: {
     paddingTop: 30,
   },
-  welcomeContainer: {
+  activateButtonContainer: {
+    marginTop: 30,
+    marginBottom: 30,
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
+  activateButton: {
+    padding: 15,
+    borderRadius: 10,
+    textAlign: 'center',
     resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+    backgroundColor: '#ffa447',
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  activateButtonText: {
+    fontFamily: 'coiny',
+    fontSize: 30,
+  }
+
+
 });
